f40119ada46c3d48c4e1f22c51907f993e84d967
calebmartin2 <calebm776@cox.net>
2021-01-10 20:34:15 -0600

    refactored calc, added point scale, removed custom

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import './index.css'
 function App() {
   const [current, setCurrent] = useState(90);
   const [final, setFinal] = useState(20);
-  const [custom, setCustom] = useState(100);
-
+  const [pointScale, setPointScale] = useState(10);
   return (
     <div className="App">
       <h1>Final Grade Calculator</h1>
@@ -18,22 +17,19 @@ function App() {
           value={current}
           onChange={(e) => setCurrent(e.target.value)}
         />
-        <br></br>
+        <br/>
         <label>Final Weight: </label>
         <input
           type="number"
           value={final}
           onChange={(e) => setFinal(e.target.value)}
         />
-        <br></br>
-        <label>Custom Goal: </label>
-        <input
-          type="number"
-          value={custom}
-          onChange={(e) => setCustom(e.target.value)}
-        />
+        <label>Point Scale: </label>
+        <br/>
+        <input type="radio" value={10} name="pointScale" onChange={(e) => setPointScale(e.target.value) } defaultChecked />10
+        <input type="radio" value={7} name="pointScale" onChange={(e) => setPointScale(e.target.value)}/>7
       </form>
-      <Calc current={current} final={final} custom={custom} />
+      <Calc current={current} final={final} pointScale={pointScale}/>
     </div>
   );
 }
--- src/Calc.js
+++ src/Calc.js
@@ -1,42 +1,27 @@
 import CalcFunc from "./CalcFunc.js";
+const gradeObjects = [
+  { gradeLetter: "A", grade10: 90, grade7: 93, color: "#2ce574" },
+  { gradeLetter: "B", grade10: 80, grade7: 85, color: "#cdf03a" },
+  { gradeLetter: "C", grade10: 70, grade7: 75, color: "#ffe500" },
+  { gradeLetter: "D", grade10: 60, grade7: 67, color: "#ff9600" },
+];
 
 function Calc(props) {
   return (
     <div className="Calc">
-      <h2 className="calc-header" style={{backgroundColor: "#2ce574", marginTop: "0.7em"}}>Custom: {props.custom === "" ? 0 : props.custom}%</h2>
-      <CalcFunc
-        custom={props.custom}
-        current={props.current}
-        final={props.final}
-      />
-
-      <h2 className="calc-header" style={{backgroundColor: "#cdf03a"}}>A 90%:</h2>
-      <CalcFunc
-        custom="90"
-        current={props.current}
-        final={props.final}
-      />
-
-      <h2 className="calc-header" style={{backgroundColor: "#ffe500"}}>B 80%:</h2>
-      <CalcFunc
-        custom="80"
-        current={props.current}
-        final={props.final}
-      />
-
-      <h2 className="calc-header" style={{backgroundColor: "#ff9600"}}>C 70%:</h2>
-      <CalcFunc
-        custom="70"
-        current={props.current}
-        final={props.final}
-      />
-
-      <h2 className="calc-header" style={{backgroundColor: "#ff3924"}}>D 60%:</h2>
-      <CalcFunc
-        custom="60"
-        current={props.current}
-        final={props.final}
-      />
+      <br />
+      {gradeObjects.map((obj, index) => (
+        <div key={index}>
+          <h2 className="calc-header" style={{ backgroundColor: obj.color }}>
+            {obj.gradeLetter} {props.pointScale === 10 ? obj.grade10 : obj.grade7}%:
+          </h2>
+          <CalcFunc
+            custom={props.pointScale === 10 ? obj.grade10 : obj.grade7}
+            current={props.current}
+            final={props.final}
+          />
+        </div>
+      ))}
     </div>
   );
 }
